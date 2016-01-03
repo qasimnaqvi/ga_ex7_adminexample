4e21c5005ab4b641b432ada0df22a2130d7638e9
drew-mahrt <drew.mahrt@generalassemb.ly>
2016-01-03 06:03:51 -0500

    updated

---
+++ src/main/java/App.java
@@ -0,0 +1,43 @@
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		//Ask the user for the answer
+		System.out.println("did you register an account? (yes/no)");
+
+		//Ask the user for the answer
+		System.out.println("did you cancel your account? (yes/no)");
+
+		//Ask the user for the answer
+		System.out.println("are you an admin? (yes/no)");
+
+		//Ask the user for the answer
+		System.out.println("are you signed in? (yes/no)");
+
+
+
+		if () //Check if registered is "no" or canceled is "yes"
+		{
+			System.out.println("go away");
+		}
+		else if () //Check if signedIn is "yes" and admin is "yes"
+		{
+			System.out.println("you can see and change all the pages");
+		}
+		else if () //Check if signedIn is "yes" and admin is "no"
+		{
+			System.out.println("you can see all the pages");
+		}
+		else if () //Check if signedIn is "no"
+		{
+			System.out.println("you can't see any of the pages, please sign in");
+		}
+		else
+		{
+			System.out.println("sorry!!!");
+		}
+	}
+}
+
+
